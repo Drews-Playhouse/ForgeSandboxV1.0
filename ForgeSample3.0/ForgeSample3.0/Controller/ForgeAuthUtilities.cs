d9e31f223332e3b3868b0dc415d1660a2126a0e7
Drew Waszak <35929280+DrewWazz@users.noreply.github.com>
2020-01-25 06:43:30 -0600

    Add files via upload

---
+++ ForgeSample3.0/ForgeSample3.0/Controller/ForgeAuthUtilities.cs
@@ -0,0 +1,46 @@
+﻿using forgeSample.Controllers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace forgeSample.Controllers
+{
+    public static class ForgeAuthUtilities
+    {
+        public static async Task<ForgeOAuthToken> GetTokenAsync(string code)
+        {
+            var localVarFormParams = new Dictionary<String, String> {
+                {"client_id", "wiIdAHuoFZJYnaCNhxL20pZG4fVkZokz" },
+                {"client_secret", "typW3w02FrkmvVKF" },
+                {"grant_type", "authorization_code" },
+                {"code", code },
+                {"redirect_uri", "http://localhost:3000/api/forge/callback/oauth" }
+            };
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue { NoCache = true };
+                client.DefaultRequestHeaders.UserAgent.Clear();
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("d3tech-fusion-sdk");
+                HttpResponseMessage httpResponseMessage = await client.PostAsync("https://developer.api.autodesk.com/authentication/v1/gettoken",
+                    new FormUrlEncodedContent(localVarFormParams));
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    // deserialize response into token
+                    var strResponseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrWhiteSpace(strResponseBody))
+                    {
+                        var token = JsonConvert.DeserializeObject<ForgeOAuthToken>(strResponseBody);
+                        return token;
+                    }
+                }
+            }
+            return new ForgeOAuthToken();
+        }
+    }
+}
---
+++ ForgeSample3.0/ForgeSample3.0/Controller/ForgeOAuthToken.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace forgeSample.Controllers
+{
+    public class ForgeOAuthToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+        private int _expiresin;
+        [JsonProperty("expires_in")]
+        public int ExpiresIn
+        {
+            get
+            {
+                return _expiresin;
+            }
+            set
+            {
+                _expiresin = value;
+                ExpiresOn = DateTimeOffset.Now.AddSeconds(value);
+            }
+        }
+        [JsonProperty("expires_on")]
+        public DateTimeOffset ExpiresOn { get; private set; }
+        public bool IsValid()
+        {
+            if (ExpiresOn > DateTimeOffset.Now)
+                return true;
+            return false;
+        }
+    }
+}
